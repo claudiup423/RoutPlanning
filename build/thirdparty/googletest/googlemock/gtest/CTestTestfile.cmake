9aaf3e0e567d5dd3a442626f4a2c9f70a4b1436f
Claudiu <claudiupop182@gmail.com>
2021-06-26 14:45:15 +0100

    First commit

---
+++ build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/claudiu/portofoliu_ws/src/Application-Development/RoutePlanning/thirdparty/googletest/googletest
+# Build directory: /home/claudiu/portofoliu_ws/src/Application-Development/RoutePlanning/build/thirdparty/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
